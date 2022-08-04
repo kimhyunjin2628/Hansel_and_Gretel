75d52cb35fe72d2b90eae72126e29508c3de1b55
tkghlvoa <tkghlvoa@naver.com>
2022-08-04 19:52:37 +0900

    no message

---
+++ Assets/UI/GUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUI : MonoBehaviour
+{
+    public GameObject Gamestart;
+    Game_Start Gamestart_Script;
+    public GameObject start_button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Gamestart_Script = Gamestart.GetComponent<Game_Start>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnGUI()
+    {
+        
+    }
+
+    public void start_button_Onclick()
+    {
+        Gamestart_Script.start_button_Onclick = true;
+        start_button.SetActive(false);
+    }
+}
